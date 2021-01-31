1157bf3ecb9a99fff6061ae26e30a4fedce6c970
taanveerahmeed <taanveerahmeed@gmail.com>
2021-01-31 13:16:15 +0600

    Basic data type and cast operation

---
+++ BasicDataType/BasicDataType/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicDataType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int a = 10;
+            bool hasVal = true;
+            Console.WriteLine(a );
+            Console.WriteLine(hasVal);
+            Console.WriteLine("The value of a is: " + a);
+            Console.WriteLine("The value of a is {0} and hasval is {1}",a,hasVal);*/
+            byte a = 8; //a cup of tea
+            int b = a; //an empty kettlee
+
+            int c = 8; //a kettlee containing a cup of tea
+            byte d = (byte)c; //an empty cup
+
+            double f = 8.5;
+            float g = 8.4f;
+            Console.WriteLine("f = {0} and g={1}", f, g);
+
+
+
+
+        }
+    }
+}
